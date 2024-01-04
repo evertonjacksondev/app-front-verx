251dafb9024f97b37523d38f2aa7066bb6e2213b
Everton Jackson <dev.evertonjackson@gmail.com>
2024-01-04 16:02:49 -0300

    feat: create component Paragraph

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ProviderGlobal } from './context/global.context.tsx'
-
+import '../src/index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
