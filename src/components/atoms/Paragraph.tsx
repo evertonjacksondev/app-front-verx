9ba20d396f087569edbed7e475da409a63335fb9
Everton Jackson <dev.evertonjackson@gmail.com>
2024-01-10 12:29:41 -0300

    feat: add error input

--- src/components/atoms/Paragraph.tsx
+++ src/components/atoms/Paragraph.tsx
@@ -2,8 +2,13 @@ import styled from "styled-components";
 
 interface IProps {
     color?: string
+    alert?: boolean
 }
 export const Paragraph = styled.p<IProps>`
 font-size:18;
-color:${(props) => props.color ? props.color : 'black' }} ;
+color:${(props) => {
+        if (props.alert) return 'red'
+        if (props.color) return props.color
+        if (!props.color) return 'black'
+    }} ;
 `
